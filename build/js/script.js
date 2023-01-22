d10242794686c9c7f49f01301850ce44ea86d0d6
alexanderglemme <113175237+alexanderglemme@users.noreply.github.com>
2023-01-22 20:29:23 +0000

    Add output span for slider and make function in script.js  to display slider value

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,9 @@
-console.log("connected");
+var slider = document.getElementById('default-range');
+var sliderOutput = document.getElementById('output-m2')
+
+sliderOutput.innerHTML = slider.value;
+
+slider.oninput = function() {
+    sliderOutput.innerHTML = this.value
+
+}
