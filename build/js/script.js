f0c9885c0572e019022686e1b135329b097ce75a
alexanderglemme <113175237+alexanderglemme@users.noreply.github.com>
2023-01-23 02:14:54 +0000

    Add function in JS that makes savings report, rearrange som html plus comments

--- build/js/script.js
+++ build/js/script.js
@@ -1,10 +1,4 @@
 
-const mediumHouse = {
-    annualKwh: 3594,
-    annualElectricityCostEuros: 1617,
-
-};
-
 //Code for the slider in solr_calc.html
 
 var solarPanelsSlider = document.getElementById('default-range');
@@ -13,7 +7,7 @@ var solarPanelsSliderOutput = document.getElementById('output-m2');
 
 solarPanelsSliderOutput.innerHTML = solarPanelsSlider.value + "m2";
 
-solarPanelsSlider.oninput = function() {
+solarPanelsSlider.oninput = function () {
     solarPanelsSliderOutput.innerHTML = this.value + "m2";
 
 }
@@ -22,9 +16,9 @@ solarPanelsSlider.oninput = function() {
 
 const houses = document.getElementsByClassName('house')
 
-for (var i = 0; i < houses.length; i++) {
-    houses[i].addEventListener('click' , calculateAnnualSavings); 
- }
+for (let i = 0; i < houses.length; i++) {
+    houses[i].addEventListener('click', calculateAnnualSavings);
+}
 
 /**
  * Calculates annual savings
@@ -37,8 +31,33 @@ function calculateAnnualSavings() {
 
     //this.id is the id of the clicked div which is set to the house's annual kwh
     let clickedHouseAnnualKwhConsumption = parseInt(this.id);
-    let annualSavedKwh =  annualOutputKwh - clickedHouseAnnualKwhConsumption;
-    
-    let annualSavingsEuros = annualSavedKwh / 0.45;
 
+    let annualSavedKwh = annualOutputKwh - clickedHouseAnnualKwhConsumption;
+
+    let annualElectricityCostEuros = clickedHouseAnnualKwhConsumption / 0.45;
+
+    let annualGeneratedEurosFromPanels = annualSavedKwh / 0.45;
+
+    let annualSavingsEuros = annualElectricityCostEuros - annualGeneratedEurosFromPanels;
+
+    reportCalculations(annualSavingsEuros)
+
+}
+
+function reportCalculations(num) {
+    let heading = document.getElementById('report-heading');
+    let paragraph = document.getElementById('report-paragraph');
+
+    heading.innerHTML = "Your total estimated annual savings comes up to: " + "€" + Math.round(num);
+    paragraph.innerHTML = `
+    Note that this is an estimate.
+    The way we calculated these figures was by; (1)
+    taking the average annual electricity consumption 
+    of your inputed household and dividing that with 
+    the average price for electicity (€0.45/kWh); 
+    (2) calculated the expected output of solar panels 
+    and multipluing that by the area of the panels that
+     you could have installed and (3) subtracting that 
+     with your estimated annual electricity cost.
+     `
 }
